40995754695f9a79dfa356c8351198f809f753bc
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-06-22 21:32:15 +0530

    database setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.DB_URL as string)
 
 const app = express()
 app.use(express.json())
